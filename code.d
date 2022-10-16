4ab685ad31a1b99fe199982cc89c989f5bcb3a54
Shania <zixuan_guo@brown.edu--global user.name zguo47>
2022-10-16 16:54:52 -0400

    new

--- code.d
+++ code.d
@@ -5,6 +5,7 @@ code.o:     file format elf64-x86-64
 Disassembly of section .text:
 
 0000000000000000 <.text>:
-   0:	68 85 13 40 00       	pushq  $0x401385
-   5:	8b 04 25 e0 61 60 00 	mov    0x6061e0,%eax
-   c:	c3                   	retq   
+   0:	48 8d 6c 24 20       	lea    0x20(%rsp),%rbp
+   5:	68 85 13 40 00       	pushq  $0x401385
+   a:	8b 04 25 e0 61 60 00 	mov    0x6061e0,%eax
+  11:	c3                   	retq   
--- code.s
+++ code.s
@@ -1,3 +1,4 @@
+lea 0x20(%rsp), %rbp
 push $0x401385
 mov 0x6061e0, %eax
 retq
